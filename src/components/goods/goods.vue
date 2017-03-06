49fc532c4e78db00866f17a63ee6b51a76eb463f
Moudicat <i@0u0b.com>
2017-03-07 00:02:08 +0800

    Added goods.

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,188 @@
 <template>
-  <div>i am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods" class="menu-item">
+          <span class="text border-1px">
+            <i v-show="item.type>0" class="icon" :class="classMap[item.type]"></i>{{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h2 class="title">{{item.name}}</h2>
+          <ul>
+            <li v-for="food in item.foods" class="food-item border-1px">
+              <div class="icon">
+                <img width="57" height="57" :src="food.icon">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+  const ERR_OK = 0;
+
+  export default {
+    prop: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        goods: []
+      };
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+      this.$http.get('/api/goods').then((res) => {
+        res = res.body;
+        if (res.code === ERR_OK) {
+          this.goods = res.data;
+        }
+      });
+    }
+  };
 </script>
 
 <style lang="scss" rel="stylesheet/scss">
+  @import "../../common/style/mixin";
 
+  .goods {
+    display: flex;
+    position: absolute;
+    top: 174px;
+    bottom: 46px;
+    width: 100%;
+    overflow: hidden;
+    .menu-wrapper {
+      flex: 0 0 80px;
+      width: 80px;
+      background-color: #f3f5f7;
+      .menu-item {
+        display: table;
+        height: 54px;
+        width: 56px;
+        line-height: 14px;
+        padding: 0 12px;
+        text-align: center;
+        .icon {
+          display: inline-block;
+          vertical-align: top;
+          width: 12px;
+          height: 12px;
+          margin-right: 2px;
+          background-size: 12px 12px;
+          background-repeat: no-repeat;
+          &.decrease {
+            @include bg-image('decrease_3');
+          }
+          &.discount {
+            @include bg-image('discount_3');
+          }
+          &.guarantee {
+            @include bg-image('guarantee_3');
+          }
+          &.invoice {
+            @include bg-image('invoice_3');
+          }
+          &.special {
+            @include bg-image('special_3');
+          }
+        }
+        .text {
+          display: table-cell;
+          width: 56px;
+          vertical-align: middle;
+          @include border-1px(rgba(7, 17, 27, .1));
+          font-size: 12px;
+        }
+      }
+    }
+    .foods-wrapper {
+      flex: 1;
+      .title {
+        height: 26px;
+        padding-left: 14px;
+        line-height: 26px;
+        border-left: 2px solid #d9dde1;
+        font-size: 12px;
+        color: rgb(147, 153, 159);
+        background: #f3f5f7;
+      }
+      .food-item {
+        display: flex;
+        margin: 18px;
+        padding-bottom: 18px;
+        @include border-1px(rgba(7, 17, 27, .1));
+        &:last-child {
+          margin-bottom: 0;
+          &:after {
+            border-top: 0;
+          }
+        }
+        .icon {
+          flex: 0 0 57px;
+          margin-right: 10px;
+        }
+        .content {
+          flex: 1;
+          .name {
+            margin: 2px 0 8px 0;
+            height: 14px;
+            line-height: 14px;
+            font-size: 14px;
+            color: rgb(7, 17, 27);
+          }
+          .desc,
+          .extra {
+            line-height: 10px;
+            font-size: 10px;
+            color: rgb(147, 153, 159);
+          }
+          .extra {
+            margin-bottom: 8px;
+          }
+          .extra {
+            &.count {
+              margin-right: 12px;
+            }
+          }
+          .price {
+            font-weight: 700;
+            line-height: 24px;
+            .now {
+              margin-right: 8px;
+              font-size: 14px;
+              color: rgb(240, 20, 20);
+            }
+            .old {
+              text-decoration-line: line-through;
+              font-size: 10px;
+              color: rgb(147, 153, 159);
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
