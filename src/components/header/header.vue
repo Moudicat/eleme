8bc06e1e3d7f7993eba80e770ab16f25715fb1ad
Moudicat <i@0u0b.com>
2017-03-06 23:15:03 +0800

    Added header components.

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -29,19 +29,40 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="180%">
     </div>
-    <div v-show="detailShow" class="detail">
-      <div class="detail-wrapper clearfix">
-        <div class="detail-main">
-          <h1 class="name">{{seller.name}}</h1>
-          <div class="star-wrapper">
-            <star :size="48" :score="seller.score"></star>
+    <transition name="fade">
+      <div v-show="detailShow" class="detail">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="(item, index) in seller.supports">
+                <i class="icon" :class="classMap[seller.supports[index].type]"></i>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
           </div>
         </div>
+        <div class="detail-close" @click="hideDetail">
+          <i class="icon-close"></i>
+        </div>
       </div>
-      <div class="detail-close" @click="hideDetail">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -220,8 +241,15 @@
       z-index: 100;
       width: 100%;
       height: 100%;
-      background: rgba(7, 17, 27, .8);
       overflow: auto;
+      background: rgba(7, 17, 27, .8);
+      backdrop-filter: blur(10px);
+      &.fade-enter-active, &.fade-leave-active {
+        transition: all .7s;
+      }
+      &.fade-enter, &.fade-leave-active {
+        top: -100%;
+      }
       .detail-wrapper {
         width: 100%;
         min-height: 100%;
@@ -239,7 +267,71 @@
             padding: 2px 0;
             text-align: center;
           }
-
+          .title {
+            display: flex;
+            width: 80%;
+            margin: 28px auto 24px;
+            .line {
+              flex: 1;
+              position: relative;
+              top: -6px;
+              border-bottom: 1px solid rgba(255, 255, 255, .2);
+            }
+            .text {
+              padding: 0 12px;
+              font-weight: 700;
+              font-size: 14px;
+            }
+          }
+          .supports {
+            width: 80%;
+            margin: 0 auto;
+            .support-item {
+              padding: 0 12px;
+              margin-bottom: 12px;
+              font-size: 0;
+              &:last-child {
+                margin-bottom: 0;
+              }
+              .icon {
+                display: inline-block;
+                width: 16px;
+                height: 16px;
+                vertical-align: top;
+                margin-right: 6px;
+                background-repeat: no-repeat;
+                background-size: 16px 16px;
+                &.decrease {
+                  @include bg-image('decrease_2');
+                }
+                &.discount {
+                  @include bg-image('discount_2');
+                }
+                &.guarantee {
+                  @include bg-image('guarantee_2');
+                }
+                &.invoice {
+                  @include bg-image('invoice_2');
+                }
+                &.special {
+                  @include bg-image('special_2');
+                }
+              }
+              .text {
+                font-size: 12px;
+                line-height: 16px;
+              }
+            }
+          }
+          .bulletin {
+            width: 80%;
+            margin: 0 auto;
+            .content {
+              padding: 0 12px;
+              line-height: 24px;
+              font-size: 12px;
+            }
+          }
         }
       }
       .detail-close {
